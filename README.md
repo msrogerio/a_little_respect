c970704442919fa9b0f2a414c646ee38312e521b
msrogerio <msrgeri9@gmail.com>
2022-10-12 14:53:53 -0500

    commit inicial

---
+++ README.md
@@ -0,0 +1,14 @@
+# A Little Respect
+
+Organização de linhas e colunas de arquivos csv para trabalhar com `r`.
+
+### Autor
+
+#### Marlon Rogério
+
+[![Twitter Badge](https://img.shields.io/badge/-@MarlonRogrio3-1ca0f1?style=flat-square&labelColor=1ca0f1&logo=twitter&logoColor=white&link=https://twitter.com/MarlonRogrio3)](https://twitter.com/MarlonRogrio3) 
+
+
+<blockquote>
+    Todos os direitos reservados.
+</blockquote>
---
+++ run.py
@@ -0,0 +1,49 @@
+import csv, os
+
+caminho = input('Informe o caminho absoluto do arquivo csv: ')
+string_doc = """ESP\t\tEPOCA\tRESP\n"""
+try:
+    with open (caminho) as arquivo:
+        dados = csv.reader(arquivo, delimiter=',')
+        for i, j in enumerate(dados):
+            if i == 0:
+                pass
+            else:
+                esp = ''
+                for l, m in enumerate (j):
+                    if l == 0:
+                        esp = m
+                    if l == 2: 
+                        string_doc += esp + '\t0\t\t' + m + '\n'
+                    if l == 3: 
+                        string_doc += esp + '\t2\t\t' + m + '\n'
+                    if l == 4: 
+                        string_doc += esp + '\t4\t\t' + m + '\n'
+                    if l == 5: 
+                        string_doc += esp + '\t6\t\t' + m + '\n'
+                    if l == 6: 
+                        string_doc += esp + '\t8\t\t' + m + '\n'
+                    if l == 7: 
+                        string_doc += esp + '\t12\t\t' + m + '\n'
+                    if l == 8: 
+                        string_doc += esp + '\t16\t\t' + m + '\n'
+                    if l == 9: 
+                        string_doc += esp + '\t20\t\t' + m + '\n'
+                    if l == 10: 
+                        string_doc += esp + '\t24\t\t' + m + '\n'
+    # print(string_doc)
+except Exception as e:
+    print(e)
+nome_documento = input('Informe o nome do documento: ')
+if '.txt' in nome_documento:
+    pass
+if '.' in nome_documento and not '.txt' in nome_documento:
+    print('Você não informou uma extensão válida')
+if not '.txt' in nome_documento:
+    nome_documento += '.txt'
+try:
+    with open(nome_documento, 'w') as txt:
+        txt.write(string_doc)
+    print(f'Documento salvo em {os.getcwd()}/{nome_documento}')
+except Exception as e:
+    print(e)
